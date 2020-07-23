56bc682a4263d7aa219784cd7cc53cde110eb8b9
PatrykOlechno <patrykoolechno@gmail.com>
2020-07-23 20:30:55 +0200

    added urls, views, models

---
+++ core/models.py
@@ -0,0 +1,49 @@
+from django.conf import settings
+from django.db import models
+from django.shortcuts import reverse
+
+CATEGORY_CHOICES = (
+    ('S', 'Stymulant'),
+    ('D', 'Depresant'),
+    ('H', 'Halucynogen')
+)
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('D', 'danger'),
+    ('S', 'secondary')
+)
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=1)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={
+            'slug': self.slug
+        })
+
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
